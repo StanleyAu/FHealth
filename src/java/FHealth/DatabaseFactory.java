d7b6ea47a72e7167a2a6f31a99c9ff91f06eef80
Stanley Au <stanace@gmail.com>
2012-03-30 23:22:47 -0400

    DatabaseFactory supports query with PreparedStatement

--- src/java/FHealth/DatabaseFactory.java
+++ src/java/FHealth/DatabaseFactory.java
@@ -9,54 +9,82 @@ import java.util.HashMap;
  * @author brian
  */
 public class DatabaseFactory {
+
     private Connection _conn = null;
     private static DatabaseFactory dbf;
-    
+
     public DatabaseFactory() {
         try {
             Class.forName("com.mysql.jdbc.Driver");
             String url = "jdbc:mysql://localhost:3306/hospital_G004";
-            _conn = DriverManager.getConnection(url,"user_G004","jwusmells");
+            _conn = DriverManager.getConnection(url, "user_G004", "jwusmells");
         } catch (SQLException e) {
             e.printStackTrace();
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
-    }    
-    
+    }
+
     public static DatabaseFactory getInstance() {
         if (dbf == null) {
             dbf = new DatabaseFactory();
         }
         return dbf;
     }
-    
+
     public Connection getConnection() {
         return _conn;
     }
-    
-    public ResultSet executeQuery(String query) {
-        Connection _conn = getConnection();
+
+    public PreparedStatement getStatement(String searchQuery)
+            throws SQLException {
+        return getConnection().prepareStatement(searchQuery);
+    }
+
+    public ArrayList query(PreparedStatement pms) {
         ResultSet rs = null;
-        PreparedStatement pms = null;
         try {
-            pms = _conn.prepareStatement(query);
             rs = pms.executeQuery();
+
+            return _query(rs);
         } catch (SQLException e) {
             e.printStackTrace();
+            return null;
+        } finally {
+            if (pms != null) {
+                try {
+                    pms.close();
+                } catch (Exception e) {
+                }
+                pms = null;
+            }
         }
-        if (rs == null) {
-            System.out.println("Result set is null");
-        }
-        return rs;
     }
-    
+
     public ArrayList query(String searchQuery) {
+        PreparedStatement pms = null;
         ResultSet rs = null;
         try {
-            //connect to DB 
-            rs = executeQuery(searchQuery);
+            pms = getStatement(searchQuery);
+            rs = pms.executeQuery();
 
+            return _query(rs);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            if (pms != null) {
+                try {
+                    pms.close();
+                } catch (Exception e) {
+                }
+                pms = null;
+            }
+        }
+    }
+
+    protected ArrayList _query(ResultSet rs) {
+        try {
             ResultSetMetaData md = rs.getMetaData();
             int columns = md.getColumnCount();
             ArrayList list = new ArrayList(50);
--- web/jsp/register.jsp
+++ web/jsp/register.jsp
@@ -1,6 +1,6 @@
 <%-- 
-    Document   : login
-    Created on : 30-Mar-2012, 2:04:52 AM
+    Document   : register
+    Created on : 30-Mar-2012, 7:25:19 PM
     Author     : Stan
 --%>
 
@@ -9,7 +9,7 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>Hospital Management Console - Login</title>
+        <title>Hospital Management Console - Register</title>
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
         <script src="../static/js/common.js"></script>
         <link rel="stylesheet" type="text/css" href="../static/css/common.css"/>
@@ -25,10 +25,22 @@
         </div>
         <div class="content">
             <form action="../login" method="POST">
+                <label for="firstname">First Name</label>
+                <input type="text" name="firstname" value=""><br />
+                <label for="lastname">Last Name</label>
+                <input type="text" name="lastname" value=""><br />
                 <label for="username">Username</label>
                 <input type="text" name="username" value=""><br/>
                 <label for="password">Password</label>
                 <input type="text" name="password" value=""><br/>
+                <label for="ohip">OHIP</label>
+                <input type="text" name="ohip" value=""><br />
+                <label for="sin">SIN</label>
+                <input type="text" name="sin" value=""><br />
+                <label for="address">Address</label>
+                <input type="text" name="address" value=""><br />
+                <label for="phone">Phone</label>
+                <input type="text" name="phone" value=""><br />
                 <div class="buttons-container">
                     <input type="submit" name="submit" value="Login" />
                 </div>
